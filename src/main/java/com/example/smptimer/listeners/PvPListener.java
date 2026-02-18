74e0a2694cb9398aa9e7b2f175fe6934ae16e761
twiceextra57863-source <twiceextra57863@gmail.com>
2026-02-18 19:04:59 +0530

    Update PvPListener.java

--- src/main/java/com/example/smptimer/listeners/PvPListener.java
+++ src/main/java/com/example/smptimer/listeners/PvPListener.java
@@ -1,6 +1,7 @@
 package com.example.smptimer.listeners;
 
 import com.example.smptimer.SMPTimerPlugin;
+import com.example.smptimer.utils.MessageUtils;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -18,7 +19,9 @@ public class PvPListener implements Listener {
 
     @EventHandler
     public void onPlayerDamage(EntityDamageByEntityEvent event) {
-        if (!plugin.isPvpEnabled() && event.getEntity() instanceof Player && 
+        // Check if PvP is disabled and it's player vs player
+        if (!plugin.isPvpEnabled() && 
+            event.getEntity() instanceof Player && 
             event.getDamager() instanceof Player) {
             
             event.setCancelled(true);
@@ -26,20 +29,38 @@ public class PvPListener implements Listener {
             Player damager = (Player) event.getDamager();
             Player victim = (Player) event.getEntity();
             
-            // Show ash particles
-            victim.getWorld().spawnParticle(
-                Particle.valueOf(plugin.getConfig().getString("particles.hit")),
-                victim.getLocation().add(0, 1, 0),
-                30, 0.5, 0.5, 0.5, 0.1
-            );
+            // Spawn particles
+            try {
+                Particle particle = Particle.valueOf(plugin.getPluginConfig()
+                    .getString("particles.pvp-block"));
+                
+                victim.getWorld().spawnParticle(
+                    particle,
+                    victim.getLocation().add(0, 1, 0),
+                    30, 0.5, 0.5, 0.5, 0.1
+                );
+            } catch (IllegalArgumentException e) {
+                // Particle not found, use default
+                victim.getWorld().spawnParticle(
+                    Particle.ASH,
+                    victim.getLocation().add(0, 1, 0),
+                    30, 0.5, 0.5, 0.5, 0.1
+                );
+            }
             
             // Play sound
-            damager.playSound(damager.getLocation(), 
-                            Sound.valueOf(plugin.getConfig().getString("sounds.hit")), 1.0f, 1.0f);
+            try {
+                Sound sound = Sound.valueOf(plugin.getPluginConfig()
+                    .getString("sounds.pvp-block"));
+                damager.playSound(damager.getLocation(), sound, 1.0f, 1.0f);
+            } catch (IllegalArgumentException e) {
+                damager.playSound(damager.getLocation(), Sound.BLOCK_LAVA_POP, 1.0f, 1.0f);
+            }
             
             // Send message
-            damager.sendMessage(plugin.getConfig().getString("messages.prefix") + 
-                               "§cPvP is not enabled until the SMP starts!");
+            damager.sendMessage(MessageUtils.colorize(
+                plugin.getPluginConfig().getString("messages.prefix") + 
+                plugin.getPluginConfig().getString("messages.pvp-blocked")));
         }
     }
 }
